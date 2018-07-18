f5fd0593ed224e854edd944540c5a27cdbed7971
Michael Drennen <michaeldrennen74@gmail.com>
2018-07-18 15:43:12 -0600

    Syntax error fixed in base test case.

--- tests/DprmcTestCase.php
+++ tests/DprmcTestCase.php
@@ -4,7 +4,7 @@ use PHPUnit\Framework\TestCase;
 
 class DprmcTestCase extends TestCase {
 
-    public function __construct( ?string $name = null, array $data = [], string $dataName = '' ) {
+    public function __construct( string $name = null, array $data = [], string $dataName = '' ) {
         parent::__construct( $name, $data, $dataName );
 
         $dotenv = new Dotenv\Dotenv( __DIR__ );
