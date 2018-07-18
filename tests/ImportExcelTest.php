e4cb24e3ac8fd66060bf02fc74bebc7b146b294e
Michael Drennen <michaeldrennen74@gmail.com>
2018-07-18 16:04:23 -0600

    Added a phpunit test that checks for a missing input file.

--- tests/ImportExcelTest.php
+++ tests/ImportExcelTest.php
@@ -20,4 +20,17 @@ class ImportExcelTest extends DprmcTestCase {
     }
 
 
+    /**
+     * @test
+     */
+    public function missingImportFileThrowsException() {
+        $this->expectException( 'Exception' );
+        $uatUrl           = getenv( 'UAT' );
+        $prodUrl          = getenv( 'PROD' );
+        $user             = getenv( 'USER' );
+        $pass             = getenv( 'PASS' );
+        $pathToImportFile = 'tests/iDoNotExist.xlsx';
+        $importExcel      = new ImportExcel( $uatUrl, $prodUrl, $user, $pass );
+        $parsedResponse   = $importExcel->run( $pathToImportFile, false );
+    }
 }
