4afcaefa48810b8345c4e6d8e918b594739e4a09
Michael Drennen <michaeldrennen74@gmail.com>
2018-07-18 15:54:29 -0600

    Eating an exception in the DprmcTestCase file so travis-ci.org can complete its tests.

--- tests/DprmcTestCase.php
+++ tests/DprmcTestCase.php
@@ -7,8 +7,14 @@ class DprmcTestCase extends TestCase {
     public function __construct() {
         parent::__construct();
 
-        $dotenv = new Dotenv\Dotenv( __DIR__ );
-        $dotenv->load();
+        try {
+            $dotenv = new Dotenv\Dotenv( __DIR__ );
+            $dotenv->load();
+        } catch ( Exception $exception ) {
+            // Eat the exception.
+            // When testing on travis-ci.org there will be no .env file.
+        }
+
     }
 
 
