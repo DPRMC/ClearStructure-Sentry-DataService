39fedca5d66379b56b0ca83dddd4ccf5ae224c30
Michael Drennen <michaeldrennen74@gmail.com>
2025-08-19 08:40:52 -0600

    Created ImportExcelGlmxRepoTrades

---
+++ src/ImportExcelGlmxRepoTrades.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace DPRMC\ClearStructure\Sentry\DataService\Services;
+
+
+/**
+ *
+ */
+class ImportExcelGlmxRepoTrades extends ImportExcel {
+    protected $sheetName       = 'RepoTrades';
+    protected $excelFilePrefix = 'sentry_glmx_repo_trades_';
+}
